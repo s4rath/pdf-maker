8b21a1415890bb81332990c9369e7ea5cf348162
s4rath <sarathj810@gmail.com>
2022-10-13 16:58:26 +0530

    slight ui change

--- lib/file_view.dart
+++ lib/file_view.dart
@@ -15,35 +15,42 @@ class FileView extends StatefulWidget {
 class _FileViewState extends State<FileView> {
   late PageController _pageController;
   int activePage = 0;
+  // int totalPage = widget.imgObjs.length;
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(viewportFraction: 0.8);
+    _pageController = PageController(viewportFraction: 1.0);
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: PageView.builder(
-            dragStartBehavior: DragStartBehavior.down,
-            itemCount: widget.imgObjs.length,
-            pageSnapping: true,
-            controller: _pageController,
-            onPageChanged: (page) {
-              if (mounted){
-                setState(() {
-                activePage = page;
-              });
-              }
-              
-            },
-            itemBuilder: (context, pagePosition) {
-              return Container(
-                  color: Color.fromARGB(37, 22, 109, 122),
-                  margin: EdgeInsets.all(10),
-                  child: Image.file(File(widget.imgObjs[pagePosition].originalPath)));
-            }),
+      body: Column(textDirection: TextDirection.ltr,
+        children: [
+          Expanded(
+            child: PageView.builder(
+                dragStartBehavior: DragStartBehavior.down,
+                itemCount: widget.imgObjs.length,
+                pageSnapping: true,
+
+                controller: _pageController,
+                onPageChanged: (page) {
+                  if (mounted){
+                    setState(() {
+                    activePage = page;
+
+                  });
+                  }
+                  
+                },
+                itemBuilder: (context, pagePosition) {
+                  return Container(
+                     
+                      // margin: EdgeInsets.all(10),
+                      child: Image.file(File(widget.imgObjs[pagePosition].originalPath)));
+                }),
+          ),
+        ],
       ),
     );
   }
